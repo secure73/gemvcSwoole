5f0e1aa990786c67175b4b3e31782406177e5893
Ali <secure73@gmail.com>
2023-08-17 22:22:11 +0200

    not working

--- server.php
+++ server.php
@@ -1,10 +1,13 @@
 <?php
 declare(strict_types=1);
-
+require('vendor/autoload.php');
+require_once('vendor/gemvc/library/src/core/GemToken.php');
+require_once('app/config.php');
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Swoole\Http\Server;
-use Gemvc\Core\GemToken;
+//use Gemvc\Core\GemToken;
+//use Gemvc\GemToken;
 
 $http = new Server("0.0.0.0", 9501);
 
@@ -14,7 +17,8 @@ $http->on(
         echo "Gemvc API is started on port 80.\n";
     }
 );
-$gemToken = new GemToken();
+
+$gemToken = new \Gemvc\Core\GemToken();
 $http->on(
     "request",
     function (Request $request, Response $response) {
